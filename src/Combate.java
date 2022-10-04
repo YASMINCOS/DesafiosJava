5e97877e564bdc923f8a0ca62500c384b694a1d0
Yasmin vitória costa <99223801+YASMINCOS@users.noreply.github.com>
2022-10-04 13:20:56 -0300

    Update Combate.java

--- src/Combate.java
+++ src/Combate.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 public class Combate {
 	
-	static Jogador atacante = null;
-	static Jogador atacado = null;
+	static Jogador jogador01= null;
+	static Jogador jogador02 = null;
 	static List<Jogador> jogadoresEmCombate = new ArrayList<>();
 	
 	public static void main(String[] args) {
@@ -19,18 +19,19 @@ public class Combate {
 		List<Jogador> listaDeEsperaJogadores = Arrays.asList(jedi, sith,cacadorRecompensa);
 		
 		
-		atacante = escolheJogadorDeLista(listaDeEsperaJogadores);
-		atacado = escolheJogadorDeLista(listaDeEsperaJogadores);
+		jogador01 = escolheJogadorDeLista(listaDeEsperaJogadores);
+		jogador02= escolheJogadorDeLista(listaDeEsperaJogadores);
 		
-		while(atacante.getPontosVida() >0 && atacado.getPontosVida() >0) {
+		while(jogador01.getPontosVida() >0 && jogador02.getPontosVida() >0) {
 			
-			atacante.ataque(atacado);
+			jogador01.ataque(jogador02);
 			
-			System.out.println("Pontos de vida atual do jogador atacado: " + atacado.getPontosVida());
-			System.out.println("Pontos de vida atual do jogador atacante: " + atacante.getPontosVida());
+			System.out.println("Pontos de vida atual do jogador atacado: " + jogador02.getPontosVida());
+			System.out.println("Pontos de vida atual do jogador atacante: " + jogador01.getPontosVida());
 			
-			if(atacado.getPontosVida() > 0) {
-				trocarPapeis(atacante, atacado);
+			if(jogador02.getPontosVida() > 0) {
+				Combate.jogador02.ataque(jogador01);
+				System.out.println("Jogadores de combate:" + jogadoresEmCombate);
 			}
 			
 		}
@@ -47,14 +48,6 @@ public class Combate {
 		
 		return escolheJogadorDeLista(listaDeEsperaJogadores);
 		
-
 	}
-
-	private static void trocarPapeis(Jogador atacante, Jogador atacado) {
-		Jogador temp = atacado;
-		Combate.atacado = atacante;
-		Combate.atacante = temp;
-		System.out.println("Jogadores de combate:" + jogadoresEmCombate);
-		
-	}
+	
 }
